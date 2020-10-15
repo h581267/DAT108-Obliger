166ff512d9eb43ffe097a21c31f1b436f0f1fe42
h581267 <54098876+h581267@users.noreply.github.com>
2020-10-15 11:08:38 +0200

    Passord fungerer flott

--- Oblig3Erik/src/handleliste/HandleServlet.java
+++ Oblig3Erik/src/handleliste/HandleServlet.java
@@ -10,101 +10,99 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.text.*;
+import javax.servlet.http.HttpSession;
 
+import org.apache.commons.text.*;
 
 @WebServlet(name = "handle", urlPatterns = { "/handle" })
 public class HandleServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;	
+	private static final long serialVersionUID = 1L;
 	private HandleListe handleliste = new HandleListe();
 	private Cookie cookie;
-	
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-				
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-		
-        List<String> strings = handleliste.getItems();	
-		
-        response.setContentType("text/html; charset=ISO-8859-1");
+		HttpSession sesjon = request.getSession(false);
 
-        PrintWriter out = response.getWriter();
+		if (sesjon == null || (boolean) sesjon.getAttribute("bool") == false) {
+			response.sendRedirect("logginn");
+		} else {
 
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset=\"ISO-8859-1\">");
-        out.println("<title>Handlelisten</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1> Min handleliste </h1>");
-        out.println("<form action=\"" + "handle" + "\" method=\"post\">");
-        out.println("<p><input type=\"submit\" value=\"Legg til\" /> Legg til vare: <input type=\"text\" name=\"vare\" /></p>");
-        out.println("<input type=\"hidden\" id=\"itemid\" name=\"handling\" value=" + "Legg" +">");
-        out.println("</form>");
-        
-        if (strings != null) {
-        
-        for (String s : strings) {
-        	out.println("<form action=\"" + "handle" + "\" method=\"post\">");
-        	out.println("<input type=\"submit\" value=\"Slett\" />"); 
-        	out.println("<input type=\"hidden\" id=\"itemid\" name=\"handling\" value=" + "Slett" +">");
-        	out.println("<input type=\"hidden\" id=\"itemid\" name=\"vare\" value=" + s +">");
-        	out.println(s + "</br></br>");
-        	out.println("</form>");
-        }
-        }
-        
-        out.println("<p>" +  "</p>");
-        out.println("</body>");
-        out.println("</html>");
-		
-		
+			List<String> strings = handleliste.getItems();
 
-		
-		
+			response.setContentType("text/html; charset=ISO-8859-1");
+
+			PrintWriter out = response.getWriter();
+
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<meta charset=\"ISO-8859-1\">");
+			out.println("<title>Handlelisten</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<h1> Min handleliste </h1>");
+			out.println("<form action=\"" + "handle" + "\" method=\"post\">");
+			out.println(
+					"<p><input type=\"submit\" value=\"Legg til\" /> Legg til vare: <input type=\"text\" name=\"vare\" /></p>");
+			out.println("<input type=\"hidden\" id=\"itemid\" name=\"handling\" value=" + "Legg" + ">");
+			out.println("</form>");
+
+			if (strings != null) {
+
+				for (String s : strings) {
+					out.println("<form action=\"" + "handle" + "\" method=\"post\">");
+					out.println("<input type=\"submit\" value=\"Slett\" />");
+					out.println("<input type=\"hidden\" id=\"itemid\" name=\"handling\" value=" + "Slett" + ">");
+					out.println("<input type=\"hidden\" id=\"itemid\" name=\"vare\" value=" + s + ">");
+					out.println(s + "</br></br>");
+					out.println("</form>");
+				}
+			}
+
+			out.println("<p>" + "</p>");
+			out.println("</body>");
+			out.println("</html>");
+
+		}
+
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		
-		if (cookie == null) {  // sjekker om det finnes cookie, legger til om ikke
+		if (cookie == null) { // sjekker om det finnes cookie, legger til om ikke
 			cookie = new Cookie("innlogget", "");
-			cookie.setMaxAge(3600);  // en time
+			cookie.setMaxAge(3600); // en time
 			response.addCookie(cookie);
 		}
-		
-		String input = request.getParameter("handling");	
+
+		String input = request.getParameter("handling");
 		String item = request.getParameter("vare");
-		
-		request.getSession().setAttribute("handling", input);	
+
+		request.getSession().setAttribute("handling", input);
 		request.getSession().setAttribute("vare", item);
-		
+
 		String handling = (String) request.getSession().getAttribute("handling");
-		String escvare = (String) request.getSession().getAttribute("vare");	
-		
+		String escvare = (String) request.getSession().getAttribute("vare");
+
 		String vare = org.apache.commons.text.StringEscapeUtils.escapeHtml4(escvare); // ufarliggjør input
-		
-		System.out.println(handling + " " +vare);  // skriver til konsoll for testing
-		
-		if (!vare.equals("")) {  // håndterer nullverdi
-		
-		if (handling.equals("Legg") && !handleliste.finnes(vare)) {
-			handleliste.addItem(vare);
-		} else if (handling.equals("Slett")) {
-			handleliste.slettItem(vare);
-		}
+
+		System.out.println(handling + " " + vare); // skriver til konsoll for testing
+
+		if (!vare.equals("")) { // håndterer nullverdi
+
+			if (handling.equals("Legg") && !handleliste.finnes(vare)) {
+				handleliste.addItem(vare);
+			} else if (handling.equals("Slett")) {
+				handleliste.slettItem(vare);
+			}
 
 		}
-		
+
 		response.sendRedirect("handle");
-		
-		
-		
-		
-		
+
 	}
 
 }
--- Oblig3Erik/src/handleliste/LoggInnServlet.java
+++ Oblig3Erik/src/handleliste/LoggInnServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import handleliste.PassordUtil;
 
@@ -47,8 +48,20 @@ public class LoggInnServlet extends HttpServlet {
 		String passord = request.getParameter("passord");
 		PassordUtil PU = new PassordUtil();
 		PrintWriter out = response.getWriter();
+		Boolean bool;
+		
 		try {
 			if (PU.sjekkPassord(passord,"4S2XMwORX4W7wXQ4gUWYkRog+22liC2DD/NIuhmnm5nW31gVotvDZpiVuLVwMQVtzkcpq7V6YwU=")) {
+				bool = true;
+				
+				HttpSession sesjon = request.getSession(false);
+	            if (sesjon != null) {
+	                sesjon.invalidate();
+	            }
+	            sesjon = request.getSession(true);
+	            sesjon.setMaxInactiveInterval(10);
+	            sesjon.setAttribute("bool", bool);
+				
 				response.sendRedirect("handle");
 			}
 			
