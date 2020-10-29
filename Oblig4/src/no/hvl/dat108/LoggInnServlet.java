cdb3123dc66603af1981307b8b7362718e5f7f62
h581267 <54098876+h581267@users.noreply.github.com>
2020-10-29 10:13:08 +0100

    Login fungerer

---
+++ Oblig4/src/no/hvl/dat108/LoggInnServlet.java
@@ -0,0 +1,70 @@
+package no.hvl.dat108;
+
+import static no.hvl.dat108.UrlMappings.LOGIN_URL;
+import static no.hvl.dat108.UrlMappings.LIST_URL;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/" + LOGIN_URL)
+public class LoggInnServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+    @EJB
+    private DeltagerDAO deltagerDAO;
+    private Passordhjelper PH;
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+        String loginMessage = "";
+        
+        if (request.getParameter("requiresLogin") != null) {
+        	loginMessage = "Forespørselen din krever pålogging. " 
+        			+ "(Du kan ha blitt logget ut automatisk)";
+        	
+        } else if (request.getParameter("invalidUsername") != null) {
+        	loginMessage = "Ugyldig brukernavn eller passord"; 
+        }
+
+        request.setAttribute("loginMessage", loginMessage);
+        
+        request.getRequestDispatcher("WEB-INF/login.jsp")
+        		.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+        String mobil = request.getParameter("mobil");
+        String passord = request.getParameter("passord");
+        
+        try {
+			
+			Deltager d = deltagerDAO.hentDeltager(Integer.parseInt(mobil));
+			
+			if (PH.validerMedSalt2(passord, d.getPassord().getSalt(), d.getPassord().getHash())) {
+				System.out.println("Innlogging stemmer");
+				response.sendRedirect("DeltakerlisteServlet");
+			}
+			else {
+				response.sendRedirect(LOGIN_URL + "?invalidUsername");
+			}
+
+		} catch (NullPointerException e) {
+			response.sendRedirect(LOGIN_URL + "?invalidUsername");
+		}
+        
+
+    }
+}
+    
---
+++ Oblig4/src/no/hvl/dat108/NewUserServlet.java
@@ -0,0 +1,49 @@
+package no.hvl.dat108;
+
+import static no.hvl.dat108.UrlMappings.*;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/nybruker")
+public class NewUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	private DeltagerDAO deltagerDAO;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String fornavn = request.getParameter("fornavn");
+		String etternavn = request.getParameter("etternavn");
+		String mobilnummerString = request.getParameter("mobil");
+		int mobilnummer = Integer.parseInt(mobilnummerString);
+		String nyttPassord = request.getParameter("passord");
+		String kjonn = request.getParameter("kjonn");
+		Passord passord = Passord.lagPassord(nyttPassord);		
+		Deltager ny = new Deltager(fornavn,etternavn,mobilnummer, kjonn,passord);	
+		
+		if(deltagerDAO.hentDeltager(mobilnummer) == null) {
+		deltagerDAO.lagreNyDeltager(ny);
+		response.sendRedirect(CONFIRM_URL);
+		}
+		
+		
+		response.sendRedirect(LOGIN_URL);
+	}
+
+}
